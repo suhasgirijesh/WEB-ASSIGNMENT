9eec89972c60d4d508db586c222a9b7636d02f19
suhasgirijesh <137255739+suhasgirijesh@users.noreply.github.com>
2023-09-13 10:51:51 +0530

    Add files via upload

---
+++ Lab_object.js
@@ -0,0 +1,14 @@
+let pet=
+{
+    type:'tiger',
+    name:'tiger',
+    legs:'four',
+    color:'white',
+    food:{
+        favorite:"bone",
+        leastfavorite:"meat",
+        allergies:"milk",
+    }
+    }
+console.log(pet);
+console.log(pet.food);
